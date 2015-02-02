a1ba2a60a3c44fca2d2d3ad6e37e414d224ec47d
unknown <poojar@INpoojar.corporate.thoughtworks.com>
2015-02-02 16:57:59 +0530

    Spliting string expression is done without testing

---
+++ rpn.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+typedef char** String;
+int split(String,String**);
+
+int evaluate(String expression) {
+	String *splittedExpr;
+	split(expression, &splittedExpr);
+	splittedExpr[i]
+}
+
+int split(String expression, String **splitted){
+	String seprator = " ";
+	String *splittedExpr = malloc(sizeof(String*)*10);
+	String token,expr[50];
+	int i=0;
+
+	strcpy(expr, expression);
+	token = strtok(expr, seprator);
+	while( token != NULL ) 
+   	{
+		splittedExpr[i++] = token;
+      	token = strtok(NULL, seprator);
+   	}
+   	*splitted = splittedExpr;
+   	splittedExpr[i] = NULL;
+   	return i;
+}
---
+++ rpnTest.c
@@ -0,0 +1,6 @@
+#include <assert.h>
+#include "rpn.h"
+
+void test_split() {
+	evaluate("3 2 +");
+}
